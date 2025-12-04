4fa402128a64fc1992b69ece8f86e0a0687b7a50
takeru1921 <sdt2595gp0013@edu.sca.ac.jp>
2025-12-04 11:55:58 +0900

    no message

---
+++ project/Trigger.cpp
@@ -0,0 +1,37 @@
+#include "Trigger.h"
+#include<DxLib.h>
+#include<sstream>
+
+namespace KeyBuffer
+{
+    static const float REPEAT_TIME = 0.5f;
+
+    static bool tiggerKeyBufferArray[0xff];
+}
+
+void KeyTrigger::Init()
+{
+    for (int i = 0; i < 0xff; i++)
+    {
+        KeyBuffer::tiggerKeyBufferArray[i] = false;
+    }
+}
+
+bool KeyTrigger::CheckTrigger(int keyCode)
+{
+    bool TriggerFlag = false;
+
+    if (CheckHitKey(keyCode) != 0)
+    {
+        if (!KeyBuffer::tiggerKeyBufferArray[keyCode])
+        {
+            TriggerFlag = true;
+        }
+        KeyBuffer::tiggerKeyBufferArray[keyCode] = true;
+    }
+    else
+    {
+        KeyBuffer::tiggerKeyBufferArray[keyCode] = false;
+    }
+    return TriggerFlag;
+}
