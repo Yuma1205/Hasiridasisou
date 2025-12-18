879e74b514bdb3bbb1eb0328b8f741afb7586595
山本秀磨 <sdt2595sc0024@edu.sca.ac.jp>
2025-12-18 11:51:04 +0900

    スタートの際にReady Goを文字として表示した（後に画像として表示する予定）

--- project/Source/PlayScene.cpp
+++ project/Source/PlayScene.cpp
@@ -5,8 +5,10 @@
 
 PlayScene::PlayScene()
 {
-	new Field(2);
-	
+    gameState = STATE_READY;
+    stateTimer = 0;
+    new Field(1);
+
 }
 
 PlayScene::~PlayScene()
@@ -15,13 +17,47 @@ PlayScene::~PlayScene()
 
 void PlayScene::Update()
 {
-	if (CheckHitKey(KEY_INPUT_T)) {
-		SceneManager::ChangeScene("TITLE");
-	}
+    switch (gameState)
+    {
+    case STATE_READY:
+        stateTimer++;
+        if (stateTimer > 90) {   // 1秒
+            gameState = STATE_GO;
+            stateTimer = 0;
+            return;
+        }
+        return;    //ゲーム更新を止める
+
+    case STATE_GO:
+        stateTimer++;
+        if (stateTimer > 30) {   // 0.5秒
+            gameState = STATE_PLAY;
+        }
+        return;    //ゲーム更新を止める
+
+    case STATE_PLAY:
+        //ここに来て初めて通常ゲームが動く
+        break;
+    }
+    if (CheckHitKey(KEY_INPUT_T)) {
+        SceneManager::ChangeScene("TITLE");
+    }
 }
 
 void PlayScene::Draw()
 {
-	DrawString(0, 0, "PLAY SCENE", GetColor(255, 255, 255));
-	DrawString(100, 400, "Push [T]Key To Title", GetColor(255, 255, 255));
+    // READY
+    if (gameState == STATE_READY) {
+        DrawString(600, 300, "Ready", GetColor(255, 255, 255));
+    }
+    // GO
+    else if (gameState == STATE_GO) {
+        DrawString(600, 300, "Go!", GetColor(255, 255, 0));
+    }
+    // 通常ゲームの描画
+    if (gameState == STATE_PLAY) {
+        // ボールやブロックの描画
+    }
+    DrawString(0, 0, "PLAY SCENE", GetColor(255, 255, 255));
+    DrawString(100, 400, "Push [T]Key To Title", GetColor(255, 255, 255));
 }
--- project/Source/PlayScene.h
+++ project/Source/PlayScene.h
@@ -4,8 +4,16 @@
 /// <summary>
 /// ゲームプレイのシーンを制御する
 /// </summary>
+enum GameStartState {
+	STATE_READY,
+	STATE_GO,
+	STATE_PLAY,
+};
 class PlayScene : public SceneBase
 {
+private:
+	GameStartState gameState;
+	int stateTimer;
 public:
 	PlayScene();
 	~PlayScene();
